9fc83a8d12c6b22649bf5ebdfede28131eb6b13b
amirhosseinmpr <amiirhossein.mpr@gmail.com>
2024-01-21 12:39:43 +0330

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const audioPlayer = document.getElementById('audioPlayer');
+
+        const playAudio = () => {
+            audioPlayer.play();
+            document.removeEventListener('click', playAudio);
+        };
+
+        document.addEventListener('click', playAudio);
+
+        return () => {
+            document.removeEventListener('click', playAudio);
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <img src='/download.jpeg' className='w-full h-52 mb-4' alt='QrCode Image' />
+            <div className='mb-10 text-center'>You can add Your QrCode</div>
+            <div className="audio-container">
+                <audio controls id="audioPlayer">
+                    <source src="/audio.mp3" type="audio/mp3" />
+                    Your browser does not support the audio tag.
+                </audio>
+            </div>
+        </div>
+    );
 }
 
 export default App;
